43bfb0e46eeb3828d113a5a447a06074cab5fafa
Adityasinh <work.adityasodha@gmail.com>
2025-05-15 20:43:51 +0530

    sync with codepen

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ document.querySelectorAll('.music-card').forEach((card, index) => {
     const playButton = card.querySelector('.play-button');
     const progressBar = card.querySelector('.progress-bar');
     
+    
     const audio = new Audio();
-    audio.src = index === 0 ? "mp3/husn.mp3"
-               : index === 1 ? "mp3/no-surprises.mp3"
-               : index === 2 ? "mp3/one-love.mp3"
-               : "mp3/tu-hain-kahan.mp3"; 
+    audio.src = index === 0 ? "https://github.com/workofaditya/Interactive-Cards/raw/refs/heads/main/mp3/husn.mp3"
+               : index === 1 ? "https://github.com/workofaditya/Interactive-Cards/raw/refs/heads/main/mp3/no-surprises.mp3"
+               : index === 2 ? "https://github.com/workofaditya/Interactive-Cards/raw/refs/heads/main/mp3/one-love.mp3"
+               : "https://github.com/workofaditya/Interactive-Cards/raw/refs/heads/main/mp3/tu-hain-kahan.mp3"; 
     let isPlaying = false;
 
     playButton.addEventListener('click', () => {
